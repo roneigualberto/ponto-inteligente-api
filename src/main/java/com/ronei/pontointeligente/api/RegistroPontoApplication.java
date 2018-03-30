cf0dd4e9369452cbd4f5e097a82f3f6423c276a9
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-03-30 10:37:13 -0400

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/ronei/pontointeligente/api/RegistroPontoApplication.java
@@ -0,0 +1,12 @@
+package com.ronei.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroPontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroPontoApplication.class, args);
+	}
+}
